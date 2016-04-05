0a018c025920192c8c8b1c98ad2eea0e4c68a55a
annayanushkevich <fishinmyhead@gmail.com>
2016-04-04 20:41:55 -0700

    initial commit

---
+++ .store_item.rb
@@ -0,0 +1,17 @@
+items = [
+{
+  type: "shoe polish"
+  cost: 4
+  msds: "do not eat"
+  },
+{
+  "type" => "organic sheep milk"
+  "cost" => 7
+  "msds" => "safe for human consumption"
+  },
+{
+  type: "weed wacker"
+  cost: 99
+  msds: "do not operate under the influence of drugs or alchohol, do not try to eat"
+  }
+]
